6af737f73374ade254779644f6f63b7410762d49
John Cao <jhcao23@gmail.com>
2020-05-23 11:52:41 -0700

    init

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco442.kt
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+    Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+    Find all the elements that appear twice in this array.
+
+    Could you do it without extra space and in O(n) runtime?
+
+    Example:
+    Input:
+    [4,3,2,7,8,2,3,1]
+
+    Output:
+    [2,3]
+     */
+    fun findDuplicates(nums: IntArray): List<Int> {
+        val res = listOf()
+        for (i = 0; i<nums.size; i++) {
+            var hash = -nums[i]
+
+
+        }
+        return res
+    }
+}
