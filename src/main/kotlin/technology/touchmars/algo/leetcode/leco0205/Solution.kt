9bed27e7a840c5bffbeac636160db62c43ed442e
John <jhcao23@gmail.com>
2020-12-21 01:11:28 -0800

    0205

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0205/Solution.kt
@@ -0,0 +1,22 @@
+package technology.touchmars.algo.leetcode.leco0205
+
+class Solution {
+
+    fun isIsomorphic(s: String, t: String): Boolean {
+        if (s.length!=t.length) return false
+        val mapS = HashMap<Int, Int>() // map chars from s to t
+        val setT = HashSet<Int>() // save occurrence of t
+        for (idx in s.indices) {
+            val charS = s[idx].toInt()
+            val charT = t[idx].toInt()
+            val match = mapS[charS]
+            if (match == null) {
+                if (setT.contains(charT)) return false
+                else setT.add(charT)
+                mapS[charS] = charT
+            } else if (match!=charT) return false
+        }
+        return true
+    }
+
+}
