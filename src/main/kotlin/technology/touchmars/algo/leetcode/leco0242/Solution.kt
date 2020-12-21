5a228c9b41ba19087738804b26942ccf81c9501a
John <jhcao23@gmail.com>
2020-12-20 16:54:00 -0800

    0438

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0242/Solution.kt
@@ -0,0 +1,26 @@
+package technology.touchmars.algo.leetcode.leco0242
+
+class Solution {
+
+    private val pFreq = IntArray(26)
+
+    private fun initP(p: String) {
+        for(c in p.chars()) {
+            pFreq[getPos(c)]++
+        }
+    }
+
+    private fun getPos(c: Int): Int = c-'a'.toInt()
+
+    private fun isFreqMatched(pFreq: IntArray): Boolean = pFreq.none { f -> f != 0 }
+
+    fun isAnagram(s: String, t: String): Boolean {
+        initP(t)
+        for (c in s.chars()) {
+            val freq = pFreq[getPos(c)]--
+            if (freq < 0) return false
+        }
+        return isFreqMatched(pFreq)
+    }
+
+}
