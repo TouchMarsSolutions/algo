c062e31b83c4f6ee317ca6341540e63650c3d0c9
John <jhcao23@gmail.com>
2020-12-20 22:12:03 -0800

    0217

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0217/Solution.kt
@@ -0,0 +1,12 @@
+package technology.touchmars.algo.leetcode.leco0217
+
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = HashSet<Int>()
+        nums.forEach { num ->
+            if (set.contains(num)) return true
+            else set.add(num)
+        }
+        return false;
+    }
+}
