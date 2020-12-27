10dc9e8637e4b72b196ed1dcba58f9e238aaf785
John <jhcao23@gmail.com>
2020-12-26 16:08:08 -0800

    0653

--- src/main/kotlin/technology/touchmars/algo/leetcode/leco0653/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0653/Solution.kt
@@ -2,57 +2,28 @@ package technology.touchmars.algo.leetcode.leco0653
 
 class Solution {
 
-    fun inOrder(root: TreeNode?, list: MutableList<Int> = mutableListOf()) {
-        if (root == null) return
+    fun inOrder(root: TreeNode?, list: MutableList<Int> = mutableListOf()): List<Int> {
+        if (root == null) return list
         inOrder(root.left, list)
         list.add(root.`val`)
         inOrder(root.right, list)
+        return list
     }
 
     fun findTarget(root: TreeNode?, k: Int): Boolean {
-        if (root==null) return false
-        if (root.left == null && root.right == null) return false
-        val rootVal = root.`val`
-        var leftTree = root
-        var rightTree = root
-        when {
-            k-rootVal > rootVal -> {
-                rightTree = root.right
-            }
-            k-rootVal < rootVal -> {
-                leftTree = root.left
-            }
-            else -> {
-                leftTree = root.left
-                rightTree = root.right
+        val list = inOrder(root)
+        if (list.size < 2) return false
+        var tail = 0
+        var head = list.lastIndex
+        while (tail < head) {
+            val sum = list[tail] + list[head]
+            when {
+                sum == k -> return true
+                sum < k -> tail++
+                else -> head--
             }
         }
-        while (leftTree!=null && rightTree!=null) {
-            val sum = leftTree.`val` + rightTree.`val`
-            if (sum == k) return true
-            else if (sum < k) { // go to right
-                if (leftTree.right!=rightTree) {
-                    leftTree = leftTree.right
-                } else if(leftTree.right == rightTree && rightTree.left!=null) {
-                    leftTree = rightTree.left
-                } else if (rightTree.right!=null) {
-                    rightTree = rightTree.right
-                } else {
-                    return false
-                }
-            } else { // sum > k => go to left
-                if (rightTree.left != null && rightTree.left != leftTree) {
-                    rightTree = rightTree.left
-                } else if (rightTree.left == leftTree && leftTree.right!=null) {
-                    rightTree = leftTree.right
-                } else if (leftTree.left!=null) {
-                    leftTree = leftTree.left
-                } else {
-                    return false
-                }
-            }
-        }
-        return leftTree!=null && rightTree!=null && leftTree.`val`+rightTree.`val`==k
+        return false
     }
 
 }
