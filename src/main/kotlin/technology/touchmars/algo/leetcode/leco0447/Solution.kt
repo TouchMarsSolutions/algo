93e9e6f698a9b384fbd96a72d35dc0462543aabc
John <jhcao23@gmail.com>
2020-12-28 02:02:05 -0800

    0447

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0447/Solution.kt
@@ -0,0 +1,25 @@
+package technology.touchmars.algo.leetcode.leco0447
+
+class Solution {
+
+    private fun distance(i: IntArray, j: IntArray): Int = distance(i[0], i[1], j[0], j[1])
+    private fun distance(x: Int, y: Int, a: Int, b: Int): Int = (x-a)*(x-a)+(y-b)*(y-b)
+
+    fun numberOfBoomerangs(points: Array<IntArray>): Int {
+        var sol = 0
+        for (p in points.indices) {
+            // map for distance -> count of points-pairs
+            val map = hashMapOf<Int, Int>()
+            for (q in points.indices) {
+                if (p!=q) {
+                    val dis = distance(points[p], points[q])
+                    map[dis] = 1 + map.getOrDefault(dis, 0)
+                }
+            }
+            // iterate distance map
+            map.values.forEach { sol+=it*(it-1) }
+        }
+        return sol
+    }
+
+}
---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0447/test.kt
@@ -0,0 +1,8 @@
+package technology.touchmars.algo.leetcode.leco0447
+
+fun main() {
+    // [[0,0],[1,0],[-1,0],[0,1],[0,-1]]
+    println(Solution().numberOfBoomerangs(arrayOf(
+        intArrayOf(0,0), intArrayOf(1,0), intArrayOf(-1,0), intArrayOf(0,1), intArrayOf(0,-1))
+    ))
+}
