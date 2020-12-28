1cc443bf451e64ec6705a4b6931232eb3a7b04cf
John <jhcao23@gmail.com>
2020-12-27 18:06:22 -0800

    0259 better combinatorics

--- src/main/kotlin/technology/touchmars/algo/leetcode/leco0259/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0259/Solution.kt
@@ -1,59 +1,55 @@
 package technology.touchmars.algo.leetcode.leco0259
 
-import java.util.*
+import java.util.SortedMap
 
 class Solution {
-
+    private fun min(a:Int, b: Int): Int = if (a<=b) a else b
     private fun initMap(nums: IntArray, map: SortedMap<Int, Int> = sortedMapOf()): SortedMap<Int, Int> {
         nums.forEach { num -> map[num] = 1 + map.getOrDefault(num, 0) }
         return map
     }
-    private fun combinatorics(n: Int, k: Int): Int {
-        return if (n<=k) 1
-        else {
-            var sol = 1
-            IntRange(k+1, n).forEach { r -> sol+=r*sol }
-            sol
-        }
+    fun combinatorics(n: Int, k: Int): Int {
+        return if (n==0||k==0||n==k) 1
+        else if (n==k+1) n
+        else if (k==1) n
+        else if (k==2) n*(n-1)/2
+        else if (k==3) n*(n-1)*(n-2)/6
+        else combinatorics(n-1, k) + combinatorics(n-1, k-1)
     }
     fun threeSumSmaller(nums: IntArray, target: Int): Int {
-        if (nums.isEmpty()) return 0
+        if (nums.size<3) return 0
         val map = initMap(nums)
+        val max = map.lastKey()
         var result = 0
-//        val min = map.firstKey()
-//        val max = map.lastKey()
         var tailMap = map
-//        var tail = min
         while (tailMap.isNotEmpty()) {
             val tail = tailMap.firstKey()
+            if (tail*3>=target) break
             val cnt = tailMap[tail]!!
-            when (tailMap.size) {
-                1 -> { // tail == max = 0
-                    if (tail==0 && cnt>=3) result+=combinatorics(cnt, 3)
-                    return result
+            if (cnt>=3 && tail*3<target) result+=combinatorics(cnt, 3) // AAA
+            if (cnt>=2) { // A, A, B
+                val comp = target - 2 * tail - 1
+                if (comp > tail && tail < max) {
+                    result += combinatorics(cnt, 2) *
+                            tailMap.tailMap(tail+1).headMap(min(comp, max)+1).values.sumBy { it }
                 }
-                else -> { // size >= 2
-                    if (cnt>=2) {
-                        // A, A, ?
-                        val comp = 0 - 2 * tail
-                        if (comp==tail && cnt>=3) { // AAA=000
-                            result += combinatorics(cnt, 3)
-                        }else if (comp>tail && tailMap.containsKey(comp)){ // A, A, B
-                            result += combinatorics(cnt, 2)*tailMap[comp]!!
-                        }
-                        // append::A, B, ? in [B, max]
-                    }
-                    // A, B, ? in [B, max]
-                    val headMap = tailMap.tailMap(tail+1) // okay since size>=2
-                    for (head in headMap.keys) {
-                        val comp = 0 - tail - head
-                        if (comp==head && headMap[comp]!!>=2) { // A, B, B
-                            result+=cnt*combinatorics(headMap[comp]!!, 2)
-                        } else if (comp>head && headMap.containsKey(comp)) // A, B, C
-                            result+=cnt*headMap[head]!!*headMap[comp]!!
-                    }
+                // append::A, B, ? in [B, max]
+            }
+            // A, B, ? in [B, max]
+            val headMap = tailMap.tailMap(tail+1) // okay since size>=2
+            for (head in headMap.keys) {
+                val cntHead = headMap[head]!!
+                // A, B, B
+                if (cntHead >= 2 && head * 2 + tail < target) {
+                    result += cnt * combinatorics(cntHead, 2)
                 }
-            } // end when
+                // A, B, C
+                val comp = target - tail - head - 1
+                if (comp > head && head < max) {
+                    result += cnt * cntHead *
+                            headMap.tailMap(head+1).headMap(min(comp, max)+1).values.sumBy { it }
+                }
+            }
             tailMap = map.tailMap(tail+1)
         }
         return result
