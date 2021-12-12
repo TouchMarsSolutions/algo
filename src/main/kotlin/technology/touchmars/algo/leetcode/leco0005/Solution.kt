917a4b335e833936e92fd8b4de2039cf094bd248
John <john@itouchmars.com>
2021-12-12 23:03:46 +0000

    test for space

--- src/main/kotlin/technology/touchmars/algo/leetcode/leco0005/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0005/Solution.kt
@@ -3,6 +3,7 @@ package technology.touchmars.algo.leetcode.leco0005
 class Solution {
     // TODO
     fun longestPalindrome(s: String): String {
+        println("input is $s")
         return s
     }
 }
