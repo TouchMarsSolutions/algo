d71a8e71358cebc2a1a56e687b50b0dac93a1420
John <john@itouchmars.com>
2021-12-13 06:15:21 +0000

    ereorgnize

--- src/main/kotlin/technology/touchmars/algo/leetcode/leco0266/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/leco0266/Solution.kt
@@ -3,12 +3,7 @@ package technology.touchmars.algo.leetcode.leco0266
 class Solution {
     fun canPermutePalindrome(s: String): Boolean {
         val map = mutableMapOf<Int, Int>()
-        s.chars().forEach { map[it] = 1 + map.getOrDefault(it, 0) }
-        var numOdd = 0
-        map.values.forEach {
-            if (it%2==1) numOdd++
-            if (numOdd>1) return false
-        }
-        return true
+        s.chars().forEach { c -> map[c] = map.getOrDefault(c, 0) + 1 }
+        return 1>= map.values.filter { v -> v%2==1 }.size
     }
 }
--- src/test/kotlin/technology/touchmars/algo/leetcode/leco0005/Test.kt
+++
@@ -1,5 +0,0 @@
-package technology.touchmars.algo.leetcode.leco0005
-
-class Test {
-    fun main() = println(Solution().longestPalindrome("aba"))
-}
