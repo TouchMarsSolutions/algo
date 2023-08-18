883abb76cde2a6fb29880d1c8bbeefe32f0405c8
John <jhcao23@gmail.com>
2023-08-18 00:51:48 -0700

    kSum

--- src/main/kotlin/technology/touchmars/algo/leetcode/q0015/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0015/Solution.kt
@@ -7,7 +7,8 @@ class Solution {
         return kSum(nums, 0, 0, 3)
     }
 
-    fun kSum(nums: IntArray, target: Int, start: Int, k: Int): List<List<Int>> {
+    // keep Long to avoid overflow
+    fun kSum(nums: IntArray, target: Long, start: Int, k: Int): List<List<Int>> {
         val length = nums.size
         if (start >= length) return listOf()
         if (length - start < k) return listOf()
@@ -16,26 +17,26 @@ class Solution {
         val sol = mutableListOf<List<Int>>()
         for (idx in start until length-k+1) {
             val value = nums[idx]
-            if (idx > start && value==nums[idx-1]) continue
+            if (idx > start && value == nums[idx-1]) continue
             val prevSol = kSum(nums, target-value, idx+1, k-1)
-            prevSol.forEach { addNewTuple(sol, nums, it, value) }
+            prevSol.forEach { addNewTuple(sol, it, value) }
         }
         return sol
     }
 
-    private fun addNewTuple(sol: MutableList<List<Int>>, nums: IntArray, tuple: List<Int>, value: Int) {
+    private fun addNewTuple(sol: MutableList<List<Int>>, tuple: List<Int>, value: Int) {
         val newTuple = mutableListOf<Int>()
         newTuple.add(value)
         newTuple.addAll(tuple)
         sol.add(newTuple)
     }
 
-    fun twoSum(nums: IntArray, target: Int, start: Int): List<List<Int>> {
+    fun twoSum(nums: IntArray, target: Long, start: Int): List<List<Int>> {
         var left = start
         var rite = nums.size-1
         val sol = mutableListOf<List<Int>>()
         while (left < rite) {
-            val sum = nums[left] + nums[rite]
+            val sum = nums[left].toLong() + nums[rite].toLong()
             when {
                 sum == target -> {
                     sol.add(listOf(nums[left], nums[rite]))
