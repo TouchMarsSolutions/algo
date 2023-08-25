e79be1ac439d90f246208bedff22ea9bdc43f927
John <jhcao23@gmail.com>
2023-08-25 14:39:53 -0700

    ListNode::override toString for debugging purpose

--- src/main/kotlin/technology/touchmars/algo/basic/ListNode.kt
+++ src/main/kotlin/technology/touchmars/algo/basic/ListNode.kt
@@ -2,4 +2,16 @@ package technology.touchmars.algo.basic
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+
+    // override toString
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append(`val`)
+        var cur = next
+        while (cur != null) {
+            sb.append("->").append(cur.`val`)
+            cur = cur.next
+        }
+        return sb.toString()
+    }
 }
