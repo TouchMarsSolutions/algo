2e31dc914d4174023d8c46065ff32516fedc012e
John <jhcao23@gmail.com>
2023-11-10 21:25:40 -0800

    0127 - optimized

--- src/main/kotlin/technology/touchmars/algo/leetcode/q0127/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0127/Solution.kt
@@ -4,36 +4,6 @@ import java.util.*
 
 class Solution {
 
-    fun ladderLength2(beginWord: String, endWord: String, wordList: List<String>): Int {
-        if (!wordList.contains(endWord)) return 0
-        val qStart = LinkedList<String>().apply { add(beginWord) }
-        val stepStart = mutableMapOf(beginWord to 1)
-        val qEnd = LinkedList<String>().apply { add(endWord) }
-        val stepEnd = mutableMapOf(endWord to 1)
-
-        while (qStart.isNotEmpty() && qEnd.isNotEmpty()) {
-            val toVisit = if (qStart.size <= qEnd.size) qStart to stepStart else qEnd to stepEnd
-            val queue = toVisit.first
-            val qSize = queue.size
-            val stepMap = toVisit.second
-            val otherStepMap = if (stepMap === stepStart) stepEnd else stepStart
-            for (i in 0 until qSize) {
-                val p = queue.poll()
-                val step = stepMap[p]!!
-                wordList.filter { similar(p, it) }.forEach {
-                    if (it in otherStepMap) {
-                        return step + (otherStepMap[it] ?: 0)
-                    } else if (it !in stepMap) {
-                        queue.add(it)
-                        stepMap[it] = step + 1
-                    }
-                }
-            }
-
-        }
-        return 0
-    }
-
     fun ladderLength(beginWord: String, endWord: String, wordList: List<String>): Int {
         if (!wordList.contains(endWord)) return 0
         val wordSet = wordList.toMutableSet()
