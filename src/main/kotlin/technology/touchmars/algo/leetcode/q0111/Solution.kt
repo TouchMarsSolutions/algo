ab54867365cf291fb3b682a1f4d4b9a28e863208
John <jhcao23@gmail.com>
2023-11-15 11:05:46 -0800

    0111

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0111/Solution.kt
@@ -0,0 +1,16 @@
+package technology.touchmars.algo.leetcode.q0111
+
+import technology.touchmars.algo.basic.TreeNode
+import kotlin.math.min
+
+class Solution {
+
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        if (root.left==null && root.right==null) return 1
+        if (root.left!=null && root.right==null) return 1 + minDepth(root.left)
+        if (root.left==null && root.right!=null) return 1 + minDepth(root.right)
+        return 1 + min(minDepth(root.left), minDepth(root.right))
+
+    }
+}
