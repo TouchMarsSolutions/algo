5d56641a5f3ccc5f7f44c19aa31f68b0c4793636
John <jhcao23@gmail.com>
2023-11-15 18:15:52 -0800

    0101

--- src/main/kotlin/technology/touchmars/algo/leetcode/q0101/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0101/Solution.kt
@@ -4,13 +4,14 @@ import technology.touchmars.algo.basic.TreeNode
 
 class Solution {
 
-    fun isSymmetric(root: TreeNode?): Boolean {
-        return root==null || symmetric(root.left, root.right)
-    }
+    fun isSymmetric(root: TreeNode?): Boolean =
+        root==null || symmetric(root.left, root.right)
 
-    fun symmetric(left: TreeNode?, rite: TreeNode?): Boolean {
-        if (left==null && rite==null) return true
-        return left?.`val`==rite?.`val` && symmetric(left?.left, rite?.right) && symmetric(left?.right, rite?.left)
-    }
+    private fun symmetric(left: TreeNode?, rite: TreeNode?): Boolean =
+        (left==null && rite==null) ||
+        (left?.`val`==rite?.`val`
+                && symmetric(left?.left, rite?.right)
+                && symmetric(left?.right, rite?.left)
+        )
 
 }
