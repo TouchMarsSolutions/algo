76245f41b9ee7a4aec7016afa97ba9250167a7b1
John <jhcao23@gmail.com>
2023-11-15 18:04:07 -0800

    0100

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0101/Solution.kt
@@ -0,0 +1,11 @@
+package technology.touchmars.algo.leetcode.q0101
+
+import technology.touchmars.algo.basic.TreeNode
+
+class Solution {
+
+    fun isSymmetric(root: TreeNode?): Boolean {
+
+    }
+
+}
