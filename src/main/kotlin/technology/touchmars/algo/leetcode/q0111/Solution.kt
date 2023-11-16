53ad5d77f9b41dd111c5c03c74567efae9da8ed1
John <jhcao23@gmail.com>
2023-11-15 22:02:16 -0800

    0111

--- src/main/kotlin/technology/touchmars/algo/leetcode/q0111/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0111/Solution.kt
@@ -9,7 +9,7 @@ class Solution {
         if (root == null) return 0
         if (root.left==null && root.right==null) return 1
         if (root.left!=null && root.right==null) return 1 + minDepth(root.left)
-        if (root.left==null && root.right!=null) return 1 + minDepth(root.right)
+        if (root.left==null) return 1 + minDepth(root.right)
         return 1 + min(minDepth(root.left), minDepth(root.right))
 
     }
