82d79bd8f0e72a3e2fef6e6e846877a2a7b2a962
John <jhcao23@gmail.com>
2023-11-21 14:01:59 -0800

    0257

--- src/main/kotlin/technology/touchmars/algo/leetcode/q0257/Solution.kt
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0257/Solution.kt
@@ -8,8 +8,8 @@ class Solution {
         val sol = mutableListOf<String>()
         if (root==null) return sol
         if (isLeaf(root)) return sol.apply { add(root.`val`.toString()) }
-        listOf(binaryTreePaths(root.left), binaryTreePaths(root.right)).forEach { it.forEach {
-            s -> sol.add(root.`val`.toString()+"->"+s)
+        listOf(binaryTreePaths(root.left), binaryTreePaths(root.right)).forEach { it.forEach { s ->
+            sol.add("${root.`val`}->$s")
         } }
         return sol
     }
