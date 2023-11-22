1aa4227616e5d6129692dab0a8fb6e543cfc5729
John <jhcao23@gmail.com>
2023-11-21 21:58:55 -0800

    0450

---
+++ src/main/kotlin/technology/touchmars/algo/leetcode/q0450/Solution.kt
@@ -0,0 +1,36 @@
+package technology.touchmars.algo.leetcode.q0450
+import technology.touchmars.algo.basic.TreeNode
+
+class Solution {
+
+    fun deleteNode(root: TreeNode?, key: Int): TreeNode? {
+        root ?: return null
+        val rv = root.`val`
+        val left = root.left
+        val rite = root.right
+        when {
+            key < rv -> root.left = deleteNode(left, key)
+            key > rv -> root.right = deleteNode(rite, key)
+            else -> {
+                if (isLeaf(root)) return null
+                if (left==null) return rite
+                if (rite==null) return left
+                val min = findMin(rite)
+                root.`val` = min.`val`
+                root.right = deleteNode(rite, min.`val`)
+            }
+        }
+        return root
+    }
+
+    private fun findMin(node: TreeNode): TreeNode {
+        var cur = node
+        while (cur.left!=null) {
+            cur = cur.left!!
+        }
+        return cur
+    }
+
+    private fun isLeaf(node: TreeNode?) = node!=null && node.left==null && node.right==null
+
+}
